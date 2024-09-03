65cff7341512fcc2dd14192ee8c0b491d09f9569
Sahil Agrawal <109281098+SahilAgr10@users.noreply.github.com>
2024-09-03 23:02:18 +0530

    Merchandize Update

--- android/app/src/main/java/com/example/primepicks_merchandize/MainActivity.java
+++ android/app/src/main/java/com/example/primepicks_merchandize/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.primepicks_merchandize;
 
-import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
+
+public class MainActivity  {
 }
